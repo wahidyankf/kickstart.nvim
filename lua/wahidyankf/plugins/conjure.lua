1133e2583b54c0ae5257ba0cc47a30bc5582a23d
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-06-29 21:49:41 +0700

    finish conjure config

--- lua/wahidyankf/plugins/conjure.lua
+++ lua/wahidyankf/plugins/conjure.lua
@@ -1,10 +1,14 @@
 return {
   'Olical/conjure',
-  ft = { 'clojure', 'fennel', 'python' }, -- etc
+  ft = { 'clojure', 'fennel' }, -- etc
   lazy = true,
   init = function()
     -- Set configuration options here
-    vim.g['conjure#debug'] = true
+    vim.g['conjure#debug'] = false
+    vim.g['conjure#client#clojure#nrepl#eval#print_options#length'] = 2000
+    vim.g['conjure#filetype#clojure'] = 'conjure.client.clojure.nrepl'
+    vim.g['conjure#filetype#fennel'] = 'conjure.client.fennel.aniseed'
+    vim.g['conjure#eval#comment_prefix'] = ';;'
   end,
 
   -- Optional cmp-conjure integration
