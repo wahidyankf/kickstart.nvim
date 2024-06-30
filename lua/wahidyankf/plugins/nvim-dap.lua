ab70445cdfc0babf1b0a7ee4362c9922acf2f0d3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-06-30 09:53:54 +0700

    init nvim-dap setup

--- lua/wahidyankf/plugins/nvim-dap.lua
+++ lua/wahidyankf/plugins/nvim-dap.lua
@@ -2,18 +2,22 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     'leoluz/nvim-dap-go',
+    'nvim-neotest/nvim-nio',
     'rcarriga/nvim-dap-ui',
     'theHamsta/nvim-dap-virtual-text',
-    'nvim-neotest/nvim-nio',
     'williamboman/mason.nvim',
   },
   config = function()
     local dap = require 'dap'
+    local dapui = require 'dapui'
 
-    require('dapui').setup()
+    dapui.setup()
     require('dap-go').setup()
 
     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
-    vim.keymap.set('n', '<space>dg', dap.run_to_cursor, { desc = '[G]o to cursor' })
+    vim.keymap.set('n', '<leader>dg', dap.run_to_cursor, { desc = '[G]o to cursor' })
+    vim.keymap.set('n', '<leader>dd', ':DapUiToggle<CR>', { desc = 'Toggle debugging [V]iew' })
+    vim.keymap.set('n', '<leader>dc', dap.reverse_continue, { desc = '[C]ontinue debugging' })
+    vim.keymap.set('n', '<leader>dr', ":lua require('dapui').open({reset = true})<CR>", { desc = '[R]eset debugging' })
   end,
 }
