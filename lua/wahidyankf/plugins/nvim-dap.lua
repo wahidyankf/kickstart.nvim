5c02fec651115bfb404bd90f0f667ec470894246
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-06-30 18:31:26 +0700

    make nvim-dap works for go

--- lua/wahidyankf/plugins/nvim-dap.lua
+++ lua/wahidyankf/plugins/nvim-dap.lua
@@ -1,23 +1,36 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    'leoluz/nvim-dap-go',
-    'nvim-neotest/nvim-nio',
     'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'leoluz/nvim-dap-go',
     'theHamsta/nvim-dap-virtual-text',
-    'williamboman/mason.nvim',
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    local dapgo = require 'dap-go'
 
     dapui.setup()
-    require('dap-go').setup()
+    dapgo.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dq', dap.terminate, { desc = 'Terminate debugging process [Q]' })
     vim.keymap.set('n', '<leader>dg', dap.run_to_cursor, { desc = '[G]o to cursor' })
-    vim.keymap.set('n', '<leader>dd', ':DapUiToggle<CR>', { desc = 'Toggle debugging [V]iew' })
-    vim.keymap.set('n', '<leader>dc', dap.reverse_continue, { desc = '[C]ontinue debugging' })
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = '[C]ontinue debugging' })
     vim.keymap.set('n', '<leader>dr', ":lua require('dapui').open({reset = true})<CR>", { desc = '[R]eset debugging' })
   end,
 }
