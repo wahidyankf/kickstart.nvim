d3ea4380966419472df9d07cb61ec6b5f48ba116
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-08-29 08:53:38 +0700

    add stash shortcut

--- lua/wahidyankf/plugins/vim-fugitive.lua
+++ lua/wahidyankf/plugins/vim-fugitive.lua
@@ -2,5 +2,6 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gg', ':Git<CR>', { desc = 'Open [G]it console' })
+    vim.keymap.set('n', '<leader>gh', ':Git stash<CR>', { desc = 'Git [S]tash' })
   end,
 }
