84cf4f03212991a13dc86ce7ae2286cd2d633dd3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-24 05:18:49 +0700

    update

--- lua/wahidyankf/plugins/toggleterm.lua
+++ lua/wahidyankf/plugins/toggleterm.lua
@@ -28,6 +28,52 @@ return {
       vim.keymap.set('n', '<bs>tx', ':ToggleTerm direction=horizontal name=default-horizontal<CR>', { desc = 'Toggle terminal horizontal ([X])' })
       vim.keymap.set('n', '<C-t><C-b>', ':ToggleTerm direction=tab name=default-tab<CR>', { desc = 'Toggle terminal Ta[B]' })
       vim.keymap.set('n', '<bs>tb', ':ToggleTerm direction=tab name=default-tab<CR>', { desc = 'Toggle terminal Ta[B]' })
+
+      -- Function to create a centered input window
+      local function centered_input(prompt, callback)
+        local width = 60
+        local height = 1
+        local buf = vim.api.nvim_create_buf(false, true)
+        local ui = vim.api.nvim_list_uis()[1]
+        local opts = {
+          relative = 'editor',
+          width = width,
+          height = height,
+          col = (ui.width - width) / 2,
+          row = (ui.height - height) / 2,
+          style = 'minimal',
+          border = 'rounded',
+        }
+        local win = vim.api.nvim_open_win(buf, true, opts)
+        vim.api.nvim_buf_set_option(buf, 'buftype', 'prompt')
+        vim.fn.prompt_setprompt(buf, prompt)
+        vim.cmd('startinsert')
+
+        -- Handle Enter key press
+        vim.keymap.set('i', '<CR>', function()
+          local input = vim.fn.getline('.')
+          vim.api.nvim_win_close(win, true)
+          vim.schedule(function()
+            callback(input:sub(#prompt + 1)) -- Remove prompt from input
+          end)
+        end, { buffer = buf, nowait = true })
+      end
+
+      -- New function to run command in terminal with centered input
+      local function run_command_in_terminal()
+        centered_input("Enter command: ", function(input)
+          if input and input ~= "" then
+            require("toggleterm").exec(input)
+          end
+        end)
+      end
+
+      -- New command to run input in terminal
+      vim.api.nvim_create_user_command('TermRun', run_command_in_terminal, {})
+
+      -- New keymaps to trigger the command
+      vim.keymap.set('n', '<C-t><C-r>', ':TermRun<CR>', { desc = 'Run command in [T]erminal' })
+      vim.keymap.set('n', '<bs>tr', ':TermRun<CR>', { desc = 'Run command in [T]erminal' })
     end,
   },
 }
