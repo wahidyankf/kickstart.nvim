e921e165f3d56513e9073085b9e498b3d079cff3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-26 21:35:07 +0700

    code format

--- after/ftplugin/fsharp.lua
+++ after/ftplugin/fsharp.lua
@@ -14,17 +14,13 @@ vim.keymap.set(
   { desc = 'F#: Quick [R]un current file', silent = true }
 )
 vim.keymap.set('n', '<localleader>rrt', ':TermExec cmd=' .. run_current_file .. 'direction=tab<CR>', { desc = 'F#: Quick [R]un current file', silent = true })
-vim.keymap.set(
-  'n',
-  '<localleader>rrf',
-  ':TermExec cmd=' .. run_current_file .. 'direction=float<CR>',
-  { desc = 'F#: Quick [R]un current file', silent = true }
-)
+vim.keymap.set('n', '<localleader>rrf', ':TermExec cmd=' .. run_current_file .. 'direction=float<CR>', { desc = 'F#: Quick [R]un current file', silent = true })
 
 -- Disable codelens for F# files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "fsharp",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'fsharp',
   callback = function()
     vim.lsp.codelens.refresh = function() end
   end,
 })
+
